cd8e63e119e80f2d0fb5caeb28740e4ac88ef9e3
Loreuxe <67767260+Loreuxe@users.noreply.github.com>
2024-04-05 12:10:10 +0200

    Added route to link
    Added different features for route in html link

--- main_route.py
+++ main_route.py
@@ -44,3 +44,48 @@ def search_form():
         if closest_match:
             closest_match = closest_match[0] # Closest match found in the route list
     return render_template('search_form.html', link=closest_match, closest_match=closest_match, routes_links=routes_links, length=get_number_articles()) # Render the template with HTML
+
+
+@app.route('/links') # Route to show all the links found about AI
+def links():
+    try: # If the file is not found we will return an empty list
+        with open(FILENAME, "r") as f:
+            links = json.load(f)
+    except FileNotFoundError:
+        links = []
+    return render_template('links.html', links=links)
+
+@app.route('/chat', methods=['GET', 'POST']) # Route to chat with an AI, see utils.py for the chat_bot function, Careful it takes some time to get the response
+def chat():
+    response = ""
+    if request.method == 'POST':
+        query = request.form.get('query') # Get the query from the form
+        response = chat_bot(query)  
+    return render_template('chat.html', response=response)  
+
+@app.route('/summarize', methods=['GET', 'POST']) # Route to summarize an article
+def summarize():
+    response = ""
+    if request.method == 'POST':
+        query = request.form.get('query') # User will input the link of the article
+        response = find_content(query) # We will get the content of the article
+        if query != "No article found": 
+            response = chat_bot("summarize: "+ response, 5)  # We will summarize the article using the chatbot function
+    return render_template('summarize.html', response=response)
+
+@app.route('/links/favourite')
+def favourites():
+    with open(FILENAME, "r") as f:
+        links = json.load(f)
+    return render_template('favourite.html', link=links[0]) # The first link in the list is the main article on the google news page, we will show it as the favourite
+
+# This route will show the content of the main article, Careful, if the article is not free, no content will be shown
+@app.route('/links/favourite/text')
+def text():
+    with open(FILENAME, "r") as f:
+        links = json.load(f)
+    text_content = find_content(links[0])  # We will get the content of the main article
+    return render_template('text.html', text_content=text_content) # We will show the content of the main article
+
+if __name__ == '__main__':
+    app.run(debug=True)
